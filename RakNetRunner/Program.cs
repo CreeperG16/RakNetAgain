a395b335a2d82966ad0f13b169c126f65454b156
Tamás Papp <pappt1216@gmail.com>
2025-06-26 23:11:45 +0200

    Some experimentation with the client and server

--- RakNetRunner/Program.cs
+++ RakNetRunner/Program.cs
@@ -1,14 +1,16 @@
 ﻿using RakNetAgain;
+using System.Net;
 
 namespace RakNetRunner;
 
 public class Program {
     public static void Main() {
-        // MainServer();
-        MainClient();
+        // StartServer();
+        StartClient();
+        // StartProxy();
     }
 
-    public static void MainServer() {
+    public static void StartServer() {
         RakServer server = new(25565) { MaxConnections = 10 };
 
         server.OnDiscovery += (endpoint) => {
@@ -45,7 +47,7 @@ public class Program {
         server.Start().Wait();
     }
 
-    public static void MainClient() {
+    public static void StartClient() {
         Console.WriteLine($"Attempting to connect client...");
         RakClient client = new(host: "127.0.0.1", port: 19132);
 
@@ -65,5 +67,72 @@ public class Program {
         };
 
         client.Connect().Wait();
+    }
+
+    public static void StartProxy() {
+        RakServer server = new(port: 8080) { MaxConnections = 1 };
+
+        Console.WriteLine($"server starting on 8080");
+
+        server.OnDiscovery += (endpoint) => {
+            Console.WriteLine($"SERVER: Ping from {endpoint} (Currently {server.Connections.Count} connections)");
+
+            ServerMessage discoveryMessage = new() {
+                GameVersion = "1.21.92",
+                ProtocolVersion = 818,
+                Port = 8080,
+                PortV6 = 8081,
+                ServerGuid = server.Guid,
+                PlayerCount = server.Connections.Count,
+                MaxPlayers = server.MaxConnections,
+            };
+
+            return discoveryMessage.ToString();
+        };
+
+        RakClient client = new(host: "127.0.0.1", port: 25565);
+
+        client.OnConnect += () => {
+            Console.WriteLine($"Finished connecting to upstream [S].");
+        };
+
+        server.OnConnect += (connection) => {
+            Console.WriteLine($"{connection.Endpoint} New connection...");
+
+            // ---
+
+            connection.OnConnect += () => {
+                Console.WriteLine($"Finished connecting to downstream [C].");
+            };
+
+            // ---
+            client.OnGamePacket += (packet) => {
+                Console.WriteLine($"Got packet from upstream server");
+                Console.WriteLine($"[{BitConverter.ToString(packet).Replace("-", " ")}]");
+                connection.SendGamePacket(packet, Frame.FramePriority.Immediate);
+            };
+
+            connection.OnGamePacket += (packet) => {
+                Console.WriteLine($"Got packet from downstream client");
+                Console.WriteLine($"[{BitConverter.ToString(packet).Replace("-", " ")}]");
+                client.SendGamePacket(packet, Frame.FramePriority.Immediate);
+            };
+
+            // ---
+            client.OnDisconnect += () => {
+                Console.WriteLine($"Upstream server requested disconnect.");
+                connection.Disconnect();
+            };
+
+            connection.OnDisconnect += () => {
+                Console.WriteLine($"Downstream client requested disconnect.");
+                client.Disconnect();
+            };
+        };
+
+        Task.WaitAll(
+            server.Start(),
+            client.Connect()
+        );
     }
 }
